2e238e8b163c48d77136bb13d8bda72f3c60ac49
ricardosilvacoelho <ricardo.coelho_017@hotmail.com>
2020-03-13 13:06:08 -0300

    Está sendo incluso alguns pontos na classe, como gets e Sets, toString.

--- Atividade_00/src/Aluno.java
+++ Atividade_00/src/Aluno.java
@@ -2,5 +2,27 @@ public class Aluno{
 	
    private String nome;
    private int id;
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+
+public Aluno(String nome, int id) {
+	this.nome = nome;
+	this.id = id;
+}
+@Override
+public String toString() {
+	return "Aluno [nome=" + nome + ", id=" + id + "]";
+}
+   
    
 }
